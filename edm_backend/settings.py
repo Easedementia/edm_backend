fe764ee412880498a0068d4de98834c31ea57ba4
Easedementia <it.easedementia@gmail.com>
2024-12-20 16:06:39 +0530

    modified settings.py

--- edm_backend/settings.py
+++ edm_backend/settings.py
@@ -82,6 +82,7 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     'https://master.d35tyda09dui4h.amplifyapp.com',
+    "https://www.easedementia.in",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
